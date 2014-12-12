aedd51eea3bc57b30f6a636dc8d2867f500adc2a
Jenny Chen <jennychen0322@gmail.com>
2014-12-12 00:40:55 -0800

    showitemvc updated

--- Ya-Swift/Ya/Ya/ShowItemViewController.swift
+++ Ya-Swift/Ya/Ya/ShowItemViewController.swift
@@ -11,16 +11,12 @@ import UIKit
 import CoreData
 
 class ShowItemViewController: UIViewController {
-    @IBOutlet weak var nameDis: UILabel!
-    
-    @IBOutlet weak var locationDis: UILabel!
-    @IBOutlet weak var detailDis: UILabel!
+
     @IBAction func backTapped(sender: AnyObject) {
         self.navigationController?.popToRootViewControllerAnimated(true)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
         
     }
     override func didReceiveMemoryWarning() {
