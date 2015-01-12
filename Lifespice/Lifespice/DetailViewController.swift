a64f6acc93dba32d6daffd0d8b8138a451249ca0
Jenny Chen <jennychen0322@gmail.com>
2015-01-12 00:39:39 -0800

    detail view countdown updated

--- Lifespice/Lifespice/DetailViewController.swift
+++ Lifespice/Lifespice/DetailViewController.swift
@@ -47,14 +47,43 @@ class DetailViewController: UIViewController {
     }
     
     func countdownDays(eventDate: NSDate) -> Int {
-        var today: NSDate = NSDate()
+        if (isToday(eventDate)) {
+            return 0
+        }
         var numDays: Int = 0
+        var today: NSDate = NSDate()
         var calendar: NSCalendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)!
         var unitFlags:NSCalendarUnit = .DayCalendarUnit
         let components = calendar.components(unitFlags, fromDate: today, toDate: eventDate, options: nil) as NSDateComponents
         numDays = components.day
+        
+        if (numDays < 0) {
+            return numDays
+        }
         return numDays + 1
+        
+        
     }
+    
+    
+    func isToday(date1: NSDate) ->Bool {
+        var today: NSDate = NSDate()
+        let calendar: NSCalendar = NSCalendar.currentCalendar()
+        var unitFlags: NSCalendarUnit = .YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit
+        let component1 = calendar.components(unitFlags, fromDate: date1)
+        let componentToday = calendar.components(unitFlags, fromDate: today)
+        if (component1.day == componentToday.day) {
+            if (component1.month == componentToday.month) {
+                if (component1.year == componentToday.year) {
+                    return true
+                }
+            }
+        }
+        return false
+        
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
